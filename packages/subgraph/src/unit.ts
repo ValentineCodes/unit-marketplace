1d9cdf91c1b62b8d59349d3903a45fb8b0557785
ValentineCodes <Valentineorga@gmail.com>
2023-04-02 09:24:43 +0100

    set up event handlers in subgraph

--- packages/subgraph/src/unit.ts
+++ packages/subgraph/src/unit.ts
@@ -1,6 +1,6 @@
 import {
-  EarningsWithdrawn as EarningsWithdrawnEvent,
-  FeesWithdrawn as FeesWithdrawnEvent,
+  EarningsWithdrawn as EarningWithdrawnEvent,
+  FeesWithdrawn as FeeWithdrawnEvent,
   ItemAuctionDisabled as ItemAuctionDisabledEvent,
   ItemAuctionEnabled as ItemAuctionEnabledEvent,
   ItemBought as ItemBoughtEvent,
@@ -14,291 +14,251 @@ import {
   OfferCreated as OfferCreatedEvent,
   OfferDeadlineExtended as OfferDeadlineExtendedEvent,
   OfferRemoved as OfferRemovedEvent,
-  OwnershipTransferred as OwnershipTransferredEvent
-} from "../generated/Unit/Unit"
-import {
-  EarningsWithdrawn,
-  FeesWithdrawn,
-  ItemAuctionDisabled,
-  ItemAuctionEnabled,
-  ItemBought,
-  ItemDeadlineExtended,
-  ItemListed,
-  ItemPriceUpdated,
-  ItemSellerUpdated,
-  ItemUnlisted,
-  OfferAccepted,
-  OfferAmountUpdated,
-  OfferCreated,
-  OfferDeadlineExtended,
-  OfferRemoved,
-  OwnershipTransferred
-} from "../generated/schema"
+} from "../generated/Unit/Unit";
+import { Earning, Fee, Listing, Offer } from "../generated/schema";
+import { Address, BigInt, Bytes, store } from "@graphprotocol/graph-ts";
 
-export function handleEarningsWithdrawn(event: EarningsWithdrawnEvent): void {
-  let entity = new EarningsWithdrawn(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.owner = event.params.owner
-  entity.token = event.params.token
-  entity.amount = event.params.amount
+const computeItemId = (nft: Address, tokenId: BigInt): Bytes => {
+  return nft.concatI32(tokenId.toI32());
+};
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+const computeOfferId = (
+  owner: Address,
+  nft: Address,
+  tokenId: BigInt
+): Bytes => {
+  return owner.concatI32(nft.toI32()).concatI32(tokenId.toI32());
+};
 
-  entity.save()
+export function handleItemListed(event: ItemListedEvent): void {
+  let listing = new Listing(
+    computeItemId(event.params.nft, event.params.tokenId)
+  );
+
+  listing.owner = event.params.owner;
+  listing.nft = event.params.nft;
+  listing.tokenId = event.params.tokenId;
+  listing.token = event.params.token;
+  listing.price = event.params.price;
+  listing.auction = event.params.auction;
+  listing.deadline = event.params.deadline;
+
+  listing.blockTimestamp = event.block.timestamp;
+
+  listing.save();
 }
 
-export function handleFeesWithdrawn(event: FeesWithdrawnEvent): void {
-  let entity = new FeesWithdrawn(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.feeOwner = event.params.feeOwner
-  entity.token = event.params.token
-  entity.amount = event.params.amount
+export function handleItemPriceUpdated(event: ItemPriceUpdatedEvent): void {
+  let listing = Listing.load(
+    computeItemId(event.params.nft, event.params.tokenId)
+  );
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  if (listing) {
+    listing.price = event.params.newPrice;
 
-  entity.save()
+    listing.save();
+  }
+}
+
+export function handleItemSellerUpdated(event: ItemSellerUpdatedEvent): void {
+  let listing = Listing.load(
+    computeItemId(event.params.nft, event.params.tokenId)
+  );
+
+  if (listing) {
+    listing.owner = event.params.newSeller;
+
+    listing.save();
+  }
+}
+
+export function handleItemUnlisted(event: ItemUnlistedEvent): void {
+  store.remove(
+    "Listing",
+    computeItemId(event.params.nft, event.params.tokenId).toString()
+  );
 }
 
 export function handleItemAuctionDisabled(
   event: ItemAuctionDisabledEvent
 ): void {
-  let entity = new ItemAuctionDisabled(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.nft = event.params.nft
-  entity.tokenId = event.params.tokenId
-  entity.fixedPrice = event.params.fixedPrice
+  let listing = Listing.load(
+    computeItemId(event.params.nft, event.params.tokenId)
+  );
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  if (listing) {
+    listing.auction = false;
+    if (event.params.fixedPrice.notEqual(BigInt.zero())) {
+      listing.price = event.params.fixedPrice;
+    }
 
-  entity.save()
+    listing.save();
+  }
 }
 
 export function handleItemAuctionEnabled(event: ItemAuctionEnabledEvent): void {
-  let entity = new ItemAuctionEnabled(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.nft = event.params.nft
-  entity.tokenId = event.params.tokenId
-  entity.startingPrice = event.params.startingPrice
+  let listing = Listing.load(
+    computeItemId(event.params.nft, event.params.tokenId)
+  );
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  if (listing) {
+    listing.auction = true;
+    if (event.params.startingPrice.notEqual(BigInt.zero())) {
+      listing.price = event.params.startingPrice;
+    }
 
-  entity.save()
-}
-
-export function handleItemBought(event: ItemBoughtEvent): void {
-  let entity = new ItemBought(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.buyer = event.params.buyer
-  entity.nft = event.params.nft
-  entity.tokenId = event.params.tokenId
-  entity.token = event.params.token
-  entity.price = event.params.price
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+    listing.save();
+  }
 }
 
 export function handleItemDeadlineExtended(
   event: ItemDeadlineExtendedEvent
 ): void {
-  let entity = new ItemDeadlineExtended(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.owner = event.params.owner
-  entity.nft = event.params.nft
-  entity.tokenId = event.params.tokenId
-  entity.oldDeadline = event.params.oldDeadline
-  entity.newDeadline = event.params.newDeadline
+  let listing = Listing.load(
+    computeItemId(event.params.nft, event.params.tokenId)
+  );
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  if (listing) {
+    listing.deadline = event.params.newDeadline;
 
-  entity.save()
+    listing.save();
+  }
 }
 
-export function handleItemListed(event: ItemListedEvent): void {
-  let entity = new ItemListed(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.owner = event.params.owner
-  entity.nft = event.params.nft
-  entity.tokenId = event.params.tokenId
-  entity.token = event.params.token
-  entity.price = event.params.price
-  entity.auction = event.params.auction
-  entity.deadline = event.params.deadline
+export function handleItemBought(event: ItemBoughtEvent): void {
+  let listing = Listing.load(
+    computeItemId(event.params.nft, event.params.tokenId)
+  );
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  let earnings = Earning.load(
+    listing!.owner.concatI32(event.params.token.toI32())
+  );
 
-  entity.save()
-}
+  if (!earnings) {
+    earnings = new Earning(
+      listing!.owner.concatI32(event.params.token.toI32())
+    );
 
-export function handleItemPriceUpdated(event: ItemPriceUpdatedEvent): void {
-  let entity = new ItemPriceUpdated(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.nft = event.params.nft
-  entity.tokenId = event.params.tokenId
-  entity.token = event.params.token
-  entity.oldPrice = event.params.oldPrice
-  entity.newPrice = event.params.newPrice
+    earnings.owner = listing!.owner;
+    earnings.token = event.params.token;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleItemSellerUpdated(event: ItemSellerUpdatedEvent): void {
-  let entity = new ItemSellerUpdated(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.nft = event.params.nft
-  entity.tokenId = event.params.tokenId
-  entity.oldSeller = event.params.oldSeller
-  entity.newSeller = event.params.newSeller
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleItemUnlisted(event: ItemUnlistedEvent): void {
-  let entity = new ItemUnlisted(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.owner = event.params.owner
-  entity.nft = event.params.nft
-  entity.tokenId = event.params.tokenId
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleOfferAccepted(event: OfferAcceptedEvent): void {
-  let entity = new OfferAccepted(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.offerOwner = event.params.offerOwner
-  entity.nft = event.params.nft
-  entity.tokenId = event.params.tokenId
-  entity.token = event.params.token
-  entity.amount = event.params.amount
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleOfferAmountUpdated(event: OfferAmountUpdatedEvent): void {
-  let entity = new OfferAmountUpdated(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.offerOwner = event.params.offerOwner
-  entity.nft = event.params.nft
-  entity.tokenId = event.params.tokenId
-  entity.token = event.params.token
-  entity.oldAmount = event.params.oldAmount
-  entity.newAmount = event.params.newAmount
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+    earnings.save();
+  }
 }
 
 export function handleOfferCreated(event: OfferCreatedEvent): void {
-  let entity = new OfferCreated(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.offerOwner = event.params.offerOwner
-  entity.nft = event.params.nft
-  entity.tokenId = event.params.tokenId
-  entity.token = event.params.token
-  entity.amount = event.params.amount
-  entity.deadline = event.params.deadline
+  let offer = new Offer(
+    computeOfferId(
+      event.params.offerOwner,
+      event.params.nft,
+      event.params.tokenId
+    )
+  );
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  offer.owner = event.params.offerOwner;
+  offer.nft = event.params.nft;
+  offer.tokenId = event.params.tokenId;
+  offer.token = event.params.token;
+  offer.amount = event.params.amount;
+  offer.deadline = event.params.deadline;
 
-  entity.save()
+  offer.blockTimestamp = event.block.timestamp;
+
+  offer.save();
+}
+
+export function handleOfferAccepted(event: OfferAcceptedEvent): void {
+  let listing = Listing.load(
+    computeItemId(event.params.nft, event.params.tokenId)
+  );
+
+  let earnings = Earning.load(
+    listing!.owner.concatI32(event.params.token.toI32())
+  );
+
+  if (!earnings) {
+    earnings = new Earning(
+      listing!.owner.concatI32(event.params.token.toI32())
+    );
+
+    earnings.owner = listing!.owner;
+    earnings.token = event.params.token;
+
+    earnings.save();
+  }
+
+  store.remove(
+    "Listing",
+    computeItemId(event.params.nft, event.params.tokenId).toString()
+  );
+
+  store.remove(
+    "Offer",
+    computeOfferId(
+      event.params.offerOwner,
+      event.params.nft,
+      event.params.tokenId
+    ).toString()
+  );
+}
+
+export function handleOfferAmountUpdated(event: OfferAmountUpdatedEvent): void {
+  let offer = Offer.load(
+    computeOfferId(
+      event.params.offerOwner,
+      event.params.nft,
+      event.params.tokenId
+    )
+  );
+
+  if (offer) {
+    offer.amount = event.params.newAmount;
+
+    offer.save();
+  }
 }
 
 export function handleOfferDeadlineExtended(
   event: OfferDeadlineExtendedEvent
 ): void {
-  let entity = new OfferDeadlineExtended(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.offerOwner = event.params.offerOwner
-  entity.nft = event.params.nft
-  entity.tokenId = event.params.tokenId
-  entity.oldDeadline = event.params.oldDeadline
-  entity.newDeadline = event.params.newDeadline
+  let offer = Offer.load(
+    computeOfferId(
+      event.params.offerOwner,
+      event.params.nft,
+      event.params.tokenId
+    )
+  );
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  if (offer) {
+    offer.deadline = event.params.newDeadline;
 
-  entity.save()
+    offer.save();
+  }
 }
 
 export function handleOfferRemoved(event: OfferRemovedEvent): void {
-  let entity = new OfferRemoved(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.nft = event.params.nft
-  entity.tokenId = event.params.tokenId
-  entity.offerOwner = event.params.offerOwner
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  store.remove(
+    "Offer",
+    computeOfferId(
+      event.params.offerOwner,
+      event.params.nft,
+      event.params.tokenId
+    ).toString()
+  );
 }
 
-export function handleOwnershipTransferred(
-  event: OwnershipTransferredEvent
-): void {
-  let entity = new OwnershipTransferred(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.previousOwner = event.params.previousOwner
-  entity.newOwner = event.params.newOwner
+export function handleEarningWithdrawn(event: EarningWithdrawnEvent): void {
+  store.remove(
+    "Earning",
+    event.params.owner.concatI32(event.params.token.toI32()).toString()
+  );
+}
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+export function handleFeeWithdrawn(event: FeeWithdrawnEvent): void {
+  let entity = new Fee(event.params.token);
 
-  entity.save()
+  entity.owner = event.params.feeOwner;
+  entity.token = event.params.token;
+
+  entity.save();
 }
